0d1d63e58b6ea2ff52e9be5bf998469aa73ef21d
iamkiraa <32187064+iamkiraa@users.noreply.github.com>
2018-03-23 17:01:43 +0800

    commit

---
+++ attendance.php
@@ -0,0 +1,5 @@
+<?php
+include('check_teacher.php'); //check if user if teacher
+include('header_teacher.php'); //load header content for teacher page
+include("connection.php"); // connection to database
+?>
